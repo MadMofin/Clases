3274afd525e5bd0540440da1b4e318ad456b632b
Mad Mofin <arthurjaziel1@live.com>
2025-02-17 23:49:01 -0600

    chore/first-commit

--- programacion_dinamica/classes/Fibonacci.ts
+++ programacion_dinamica/classes/Fibonacci.ts
@@ -1,23 +1,25 @@
 export class Fibonacci {
   public result: number;
+
   private first: number;
+
   private second: number;
 
   constructor(number: number) {
-    this.result = this.fibonacci(number);
+    this.setResult(this.fibonacci(number));
   }
 
   private fibonacci = (number: number): number => {
     if (number <= 1) return number;
 
-    this.first = 0;
-    this.second = 1;
+    this.setFirst(0);
+    this.setSecond(1);
 
     this.setResult(this.first + this.second);
 
     for (let i = 2; i < number; i++) {
-      this.first = this.second;
-      this.second = this.result;
+      this.setFirst(this.second);
+      this.setSecond(this.result);
       this.setResult(this.first + this.second);
     }
 
@@ -27,4 +29,8 @@ export class Fibonacci {
   public getResult = () => this.result;
 
   private setResult = (result: number) => (this.result = result);
+
+  private setFirst = (first: number) => (this.first = first);
+
+  private setSecond = (second: number) => (this.second = second);
 }
